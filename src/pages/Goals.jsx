504afab574dd38811e9d34a3c7b58468292a58af
Manri <osnolax2x@gmail.com>
2025-10-07 15:24:10 -0500

    newdashboard

---
+++ src/pages/Goals.jsx
@@ -0,0 +1,8 @@
+export default function Goals() {
+  return (
+    <div>
+      <h1>🎯 Página de Metas</h1>
+      <p>Aquí verás tus objetivos principales.</p>
+    </div>
+  );
+}
---
+++ src/pages/Habits.jsx
@@ -0,0 +1,8 @@
+export default function Habits() {
+  return (
+    <div>
+      <h1>🕒 Página de Hábitos</h1>
+      <p>Aquí irán tus hábitos de estudio.</p>
+    </div>
+  );
+}
