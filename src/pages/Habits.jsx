8ccadc0c06d946afd50ce3b8fa16e6752cd23dc3
Manri <osnolax2x@gmail.com>
2025-10-23 10:06:30 -0500

    deeed

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -1,8 +1,8 @@
 export default function Habits() {
   return (
     <div>
-      <h1>🕒 HABITS PAGE</h1>
+      <h1>🕒 PAGINA DE HABITOSS</h1>
       <p>Aquí irán tus hábitos de estudio.</p>
-    </div>
+        </div>
   );
 }
